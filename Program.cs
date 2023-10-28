425ce84cfd1b4dd4d23acf6b053efe94ed1b1fb9
HidanYugakure <fidants@code.edu.az>
2023-10-29 00:08:13 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿#region task1
+//Verilmiş n - ədədinin 3-ə və 7-ə bölünüb-bölünməməsini tapın.
+//int num1 = 42;
+//bool NumberDivision(int num)
+//{
+//    return num % 21 == 0 ? true : false;
+//}
+//Console.WriteLine(NumberDivision(num1));
+
+#endregion
+
+#region task2
+//int n = int.Parse(Console.ReadLine());
+//int m = int.Parse(Console.ReadLine());
+//string MultiplicationOfThem(int num1, int num2)
+//{
+
+//    if (num1 % 2 == 0 && num2 % 2 == 0)
+//    {
+//        int result = num1 + num2;
+//        return ($"bu ededin cemi {result} ");
+//    }
+//    return "cut deyil";
+//}
+//Console.WriteLine(MultiplicationOfThem(n, m));
+#endregion
+
+#region task3
+//int num1 = 8;
+//int num2 = 20;
+
+//string cemle(int num, int num1)
+//{
+//    int count = 0;
+//    for (int i = num; i <=num1; i++)
+//    {
+//        if (num < num1)
+//        {
+//            if (i % 2 == 1)
+//            {
+//                count++;
+//            }
+//        }
+//    }
+//    return ($"arasindaki reqem sayi{count}");
+
+//}
+//Console.WriteLine(cemle(num1,num2));
+#endregion
+#region
+
+//string OddSum(int n, int m)
+//{
+//    int oddSum = default(int);
+//    if (m > n)
+//    {
+//        for (int i = n; i <= m; i++)
+//        {
+//            if (i % 2 == 1)
+//            {
+//                oddSum += i;
+//            }
+//        }
+//        return $"{n} ve {m} ededleri arasindaki tek ededlerin cemi {oddSum} olur";
+//    }
+//    return $"1-ci eded 2-ci ededden kicikdir";
+//}
+//string oddSumResult = OddSum(10, 15);
+//Console.WriteLine(oddSumResult);
+
+#endregion
